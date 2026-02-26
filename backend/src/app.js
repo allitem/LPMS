c5cec666c3d5907c4209b26bc5a496be757c9007
Rufio Dinoto(Musk) <244thanva@gmail.com>
2026-02-26 10:14:30 +0700

    Set up Express app with CORS and routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const cors = require("cors");
 
+const authRoutes = require("./routes/auth.routes");
+const projectRoutes = require("./routes/project.routes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/projects", projectRoutes);
+
+module.exports = app;
